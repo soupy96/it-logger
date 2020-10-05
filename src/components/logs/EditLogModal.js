80017763638455a590a32d801569d985de87f36a
Michael Campbell <michael.campbell14@gmail.com>
2020-10-05 18:01:01 -0400

    lesson 80, edit and tech modal components

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-const AddLogModal = () => {
+const EditLogModal = () => {
     const [message, setMessage] = useState('');
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState('');
 
     const onSubmit = () => {
-        if(message === '' || tech === '') {
+        if (message === '' || tech === '') {
             M.toast({ html: 'Please enter a message and tech' })
         } else {
             console.log(message, tech, attention);
@@ -20,7 +20,7 @@ const AddLogModal = () => {
     }
 
     return (
-        <div id="add-log-modal" className="modal" style={modalStyle}>
+        <div id="edit-log-modal" className="modal" style={modalStyle}>
             <div className="modal-content">
                 <h4>Enter System Log</h4>
                 <div className="row">
@@ -31,7 +31,7 @@ const AddLogModal = () => {
                         </label>
                     </div>
                 </div>
-                
+
                 <div className="row">
                     <div className="input-field">
                         <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)} >
@@ -66,4 +66,4 @@ const modalStyle = {
     height: '75%'
 }
 
-export default AddLogModal
+export default EditLogModal
