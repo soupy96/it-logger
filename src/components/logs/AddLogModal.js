e7358918ba4ef7f7298d1231e7f40be7696f5d45
Michael Campbell <michael.campbell14@gmail.com>
2020-10-08 13:38:56 -0400

    lesson 89, tech options select component

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { addLog } from '../../actions/logActions'
+import TechSelectOptions from '../techs/TechSelectOptions'
 
 import M from 'materialize-css/dist/js/materialize.min.js'
 
@@ -49,9 +50,7 @@ const AddLogModal = ({ addLog }) => {
                     <div className="input-field">
                         <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)} >
                             <option value="" disabled>Select Technician</option>
-                            <option value="John Doe">John Doe</option>
-                            <option value="Sam Smith">Sam Smith</option>
-                            <option value="Sara Wilson">Sara Wilson</option>
+                            <TechSelectOptions />
                         </select>
                     </div>
                 </div>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { updateLog } from '../../actions/logActions'
+import TechSelectOptions from '../techs/TechSelectOptions'
 
 import M from 'materialize-css/dist/js/materialize.min.js'
 
@@ -54,9 +55,7 @@ const EditLogModal = ({ updateLog, current }) => {
                     <div className="input-field">
                         <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)} >
                             <option value="" disabled>Select Technician</option>
-                            <option value="John Doe">John Doe</option>
-                            <option value="Sam Smith">Sam Smith</option>
-                            <option value="Sara Wilson">Sara Wilson</option>
+                            <TechSelectOptions />
                         </select>
                     </div>
                 </div>
